24dd627ef82415c54091d2ca9c9c3272772a66e9
alyucu6 <allisonycui@gmail.com>
2023-07-17 15:33:47 -0700

    mlp changes

--- mlp.py
+++ mlp.py
@@ -12,17 +12,17 @@ import tensorflow.keras.datasets.mnist as MNIST
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, accuracy_score
-import warnings
 
-# set random seed for reproducability
 torch.manual_seed(4460)
 np.random.seed(4460)
+
 
 # LOAD AND DISPLAY MNIST
 (image_MNIST_train_set, label_MNIST_train_set), (image_MNIST_test_set, label_MNIST_test_set) = MNIST.load_data() # images are represented by a 2d array
 
+
 # DATA PREPROCESSING
-# split into training, validation, test using train_test_split from scikit-learn (using what MNIST originally allocated as the 10k test set as our entire dataset)
+# split into training, validation, test using train_test_split
 indices_train, indices_else = train_test_split(range(len(image_MNIST_test_set)), test_size=0.2) # 80% train, 20% validation and test
 
 image_train = image_MNIST_test_set[indices_train, :, :] # index, row, column
@@ -36,8 +36,7 @@ label_validation = label_else[indices_validation]
 image_test = image_else[indices_test, :, :]
 label_test = label_else[indices_test]
 
-# reformat images and labels so they can be fed into data loader
-# convert numpy array to pytorch tensor + reshape 
+# reformat images and labels so they can be fed into data loader, convert numpy array to pytorch tensor + reshape 
 image_train_torch = torch.from_numpy(image_train).type(torch.FloatTensor).view(-1,1,28,28) # -1 means size of that dimension will be automatically inferred, 1 along second dim (single channel image), height and width of 28x28 pixels
 label_train_torch = torch.from_numpy(label_train).type(torch.LongTensor)
 
@@ -48,14 +47,15 @@ image_test_torch = torch.from_numpy(image_test).type(torch.FloatTensor).view(-1,
 label_test_torch = torch.from_numpy(label_test).type(torch.LongTensor)
 
 # dataset and dataloader
-train_dataset = TensorDataset(image_train_torch, label_train_torch) # uses TensorDataset (input data + corresponding labels)
-train_dataloader = DataLoader(train_dataset, batch_size=1000) # DataLoader responsible for batching data during training
+train_dataset = TensorDataset(image_train_torch, label_train_torch)   # TensorDataset (input data + corresponding labels)
+train_dataloader = DataLoader(train_dataset, batch_size=1000)         # DataLoader for batching data
 
 val_dataset = TensorDataset(image_validation_torch, label_validation_torch)
 val_dataloader = DataLoader(val_dataset, batch_size=1000)
 
 test_dataset = TensorDataset(image_test_torch, label_test_torch)
 test_dataloader = DataLoader(test_dataset, batch_size=1000)
+
 
 # MLP MODEL IMPLEMENTATION
 '''
@@ -82,6 +82,7 @@ our_MLP = MLPModel()
 
 optimizer = SGD(our_MLP.parameters(), lr = 0.1)
 
+
 # TRAIN MLP MODEL FOR CLASSIFICATION
 '''
 1. define number of epochs 
@@ -95,7 +96,6 @@ optimizer = SGD(our_MLP.parameters(), lr = 0.1)
 '''
 
 EPOCHS = 400
-
 train_epoch_loss = []
 val_epoch_loss = []
 
@@ -128,3 +128,30 @@ for epoch in range(EPOCHS):
     torch.save(our_MLP.state_dict(), './saved_MLP_models/checkpoint_epoch_%s.pth' % (epoch)) 
         
 
+# VALIDATE DATA
+for batch_index, (train_image, train_label) in enumerate(train_dataloader):
+    train_label_predicted = our_MLP(train_image)
+
+# check which epoch model performs best on validation set, use for inference on test set
+best_epoch = np.argmin(val_epoch_loss)
+print(best_epoch)
+state_dict = torch.load('./saved_MLP_models/checkpoint_epoch_%s.pth' % (best_epoch))
+our_MLP.load_state_dict(state_dict)
+
+
+# PREDICT LABELS ON TEST SET
+def predict_with_pytorch(model, input_data):
+    model.eval()
+    label_predicted_all = []
+
+    label_predicted_one_hot = model(input_data)
+    label_predicted_probability, label_predicted_index = torch.max(label_predicted_one_hot.data, 1)
+    
+    for current_prediction in label_predicted_index:
+        label_predicted_all.append(current_prediction.detach().cpu().numpy().item())
+
+    return label_predicted_all
+
+test_label_predicted = predict_with_pytorch(our_MLP, image_test_torch)
+accuracy = accuracy_score(label_test, test_label_predicted)
+print("Accuracy:", accuracy * 100, "%")
